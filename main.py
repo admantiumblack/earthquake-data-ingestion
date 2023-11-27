4cb8ce6dcf81d9b5fe23b73fe0ad4a52c1390635
auto_linter_action <felixroland12355@users.noreply.github.com>
2023-11-27 08:27:48 +0000

    Automatic Code Formatting

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import argparse
 import os
 from datetime import datetime
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog='earthquake_ingestion',
-        description='ingestion layer for USGS earthquake API'
+        prog="earthquake_ingestion",
+        description="ingestion layer for USGS earthquake API",
     )
-    parser.add_argument('start_date')
-    parser.add_argument('end_date')
-    parser.add_argument('-b', '--bucket', default=os.environ.get("DEFAULT_BUCKET"))
-    parser.add_argument('-f', '--format', default='parquet', choices=['parquet', 'csv', 'excel'])
-    parser.add_argument('-n', '--name', default=f'{datetime.now()}')
+    parser.add_argument("start_date")
+    parser.add_argument("end_date")
+    parser.add_argument("-b", "--bucket", default=os.environ.get("DEFAULT_BUCKET"))
+    parser.add_argument(
+        "-f", "--format", default="parquet", choices=["parquet", "csv", "excel"]
+    )
+    parser.add_argument("-n", "--name", default=f"{datetime.now()}")
