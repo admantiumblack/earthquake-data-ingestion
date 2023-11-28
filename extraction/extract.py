67db79a738aed18ec4e5d003042284f54f68bee5
Roland <felixroland12355@gmail.com>
2023-11-28 16:18:22 +0700

    Update extract.py
    add usage of default param as geojson

--- extraction/extract.py
+++ extraction/extract.py
@@ -4,4 +4,4 @@ import os
 
 def get_earthquake_data_source():
     url = os.environ["EARTHQUAKE_URL"]
-    return APIDataSource(url)
+    return APIDataSource(url, default_param={'format':'geojson'})
