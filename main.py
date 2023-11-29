3b5d40ccdcff300d531491990d50de8cfa95d2e9
Roland <felixroland12355@gmail.com>
2023-11-29 15:12:37 +0700

    change main to improve testability

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from datetime import datetime
 from extraction import get_data_source
 
+def main(arguments):
+    api_source = get_data_source('earthquake')
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="earthquake_ingestion",
@@ -10,10 +13,11 @@ if __name__ == "__main__":
     )
     parser.add_argument("start_date")
     parser.add_argument("end_date")
+    parser.add_argument('-t', '--type', default='earthquake')
     parser.add_argument("-b", "--bucket", default=os.environ.get("DEFAULT_BUCKET"))
     parser.add_argument(
         "-f", "--format", default="parquet", choices=["parquet", "csv", "excel"]
     )
     parser.add_argument("-n", "--name", default=f"{datetime.now()}")
     
-    api_source = get_data_source('earthquake')
+    main(parser.parse_args())
