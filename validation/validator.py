5da19e8086adbda9d658b0efd09340ddc4198334
Roland <felixroland12355@gmail.com>
2023-12-04 15:07:11 +0700

    implement validator

---
+++ validation/validator.py
@@ -0,0 +1,11 @@
+import json
+from cerberus import Validator
+from utils.path_parser import resolve_path
+
+def create_validator(schema:str|dict):
+    if isinstance(schema, str):
+        file_name = resolve_path(f"validation/schema/{schema}.json")
+        with open(file_name, "r") as f:
+            schema = json.load(f)
+    
+    return Validator(schema)
