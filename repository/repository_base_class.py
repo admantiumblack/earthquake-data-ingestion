f09dd95c9a0a108fc7b7051e1298c113a3afc1f5
Roland <felixroland12355@gmail.com>
2023-12-15 16:02:55 +0700

    create repository base class

---
+++ repository/repository_base_class.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class RepositoryBaseClass(ABC): #pragma: no cover
+
+    def __init__(self, bucket):
+        self.bucket = bucket
+    
+    @abstractmethod
+    def save(self, data:pd.DataFrame, file_name, file_type):
+        raise NotImplementedError()
