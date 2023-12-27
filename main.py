8b10f30429d0c1ae36b5d26dc5f8d157b8e5053f
Roland <felixroland12355@gmail.com>
2023-12-27 14:26:54 +0700

    Update main.py
    integrate all components

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import argparse
 import os
 from datetime import datetime
+from extraction import get_data_source
+from validation import create_validator
+from process import Pipeline
+from repository import GCSRepository
 
 
 def main(arguments):
-    pass
-    # api_source = get_data_source(arguments.type)
-    # validator = create_validator(arguments.type)
-    # api_source.validator = validator
-    # parameter = api_source.construct_parameter(arguments)
-    # data = api_source.query_source(parameter).validate().clean_data
-    # pipeline = Pipeline(arguments.type)
-    # result = pipeline.run(data)
+    api_source = get_data_source(arguments.type)
+    validator = create_validator(arguments.type)
+    api_source.validator = validator
+    parameter = api_source.construct_parameter(arguments)
+    data = api_source.query_source(parameter).validate().clean_data
+    pipeline = Pipeline(arguments.type)
+    result = pipeline.run(data)
+    repository = GCSRepository(arguments.bucket)
+    repository.save(result, arguments.name, arguments.format)
+    
 
 
 if __name__ == "__main__":
